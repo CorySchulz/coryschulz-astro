ecfa0c1f65d7d8f75bad6f8e3bef01c10a1020d9
Cory Schulz <coryschulz@gmail.com>
2025-07-20 16:46:42 -0500

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import vercel from '@astrojs/vercel/static';
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  build: {
+    format: 'file'
+  }
 });
