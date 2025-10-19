81c65a13b958c4107d94e06ad83d2ec740781a1e
Cory Schulz <coryschulz@gmail.com>
2025-10-19 11:04:48 -0500

    fiixng build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
+import { fileURLToPath } from 'url';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@magic-spells/tarot': fileURLToPath(new URL('./src/scripts/tarot.esm.js', import.meta.url))
+      }
+    }
   },
   build: {
     format: 'file'
